b7385ce4aea394eeb0f6f16462983ccccf36bfa4
ujjwalbharadwaj <ujjwalbhardwaj5809@gmail.com>
2024-04-09 16:37:48 +0530

    minor change in footer

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -17,18 +17,23 @@ const Footer = () => {
     <div className="bg-[#000] text-white">
       <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#342ac8] py-7">
         <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
-        <span className="text-[#1b602e]">Live chat </span>with us for queries{" "}
+          <span className="text-[#1b602e]">Live chat </span>with us for queries{" "}
           <br />
           events and offers
         </h1>
         <div>
-        <a href="https://wa.me/917277244691" target="_blank"><img src='whatsappclick.png' width='350px'
-          /></a>
+          <a href="https://wa.me/917277244691" target="_blank">
+            <img src="whatsappclick.png" width="300px" />
+          </a>
         </div>
       </div>
       <div className="grid grid-cols-1 sm:gird-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16 sm:text-center">
         <ul className="px-5 text-center sm:text-start flex sm:block flex-col items-center">
-        <h1 style={{ color: '#142337', fontSize: '44px', fontWeight: 'bold' }}>vaymp</h1>
+          <h1
+            style={{ color: "#142337", fontSize: "44px", fontWeight: "bold" }}
+          >
+            vaymp
+          </h1>
           <br />
           <p>The home and elements needeed to create beatiful products.</p>
           <div className="flex items-center mt-[15px]">
@@ -50,7 +55,7 @@ const Footer = () => {
 
         <ul className="text-center sm:text-start">
           <h1 className="mb-1 font-semibold">Company</h1>
-          {footerProductLinks.map((link,index) => (
+          {footerProductLinks.map((link, index) => (
             <li key={index}>
               <Link
                 className="text-gray-400 hover:text-teal-400 duration-300
@@ -65,7 +70,7 @@ const Footer = () => {
 
         <ul className="text-center sm:text-start">
           <h1 className="mb-1 font-semibold">Shop</h1>
-          {footercompanyLinks.map((link,index) => (
+          {footercompanyLinks.map((link, index) => (
             <li key={index}>
               <Link
                 className="text-gray-400 hover:text-teal-400 duration-300
@@ -80,7 +85,7 @@ const Footer = () => {
 
         <ul className="text-center sm:text-start">
           <h1 className="mb-1 font-semibold">Support</h1>
-          {footerSupportLinks.map((link,index) => (
+          {footerSupportLinks.map((link, index) => (
             <li key={index}>
               <Link
                 className="text-gray-400 hover:text-teal-400 duration-300
@@ -98,7 +103,7 @@ const Footer = () => {
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10
          text-center pt-2 text-gray-400 text-sm pb-8"
       >
-        <span>© 2020 Becodemy. All rights reserved.</span>
+        <span>© 2024 Vaymp. All rights reserved.</span>
         <span>Terms · Privacy Policy</span>
         <div className="sm:block flex items-center justify-center w-full">
           <img
